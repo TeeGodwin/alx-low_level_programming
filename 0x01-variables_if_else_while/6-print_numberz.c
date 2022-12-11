847d33dc0787832815d928859d2124cc2d9d4137
TeeGodwin <toksgodwin@gmail.com>
2022-12-10 17:09:57 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return single integer numbers from 0 - 10
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
