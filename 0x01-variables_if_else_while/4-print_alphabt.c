9cc64bf50db7fcd213bb1122968701b366756f00
TeeGodwin <toksgodwin@gmail.com>
2022-12-12 12:14:05 -0800

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,19 +12,17 @@ int main(void)
 
 {
 	char ch = 'a';
+
 	while (ch <= 'z')
 	{
-
-		if ((ch != 'e') && (ch != 'q')
+		if ((ch != 'e') && (ch != 'q'))
 		{
-		
-		       putchar(ch);
+			putchar(ch);
 
 		}
-
 		ch++;
-
-   	}
+	
+	}
 	putchar('\n');
 	return (0);
-}	
+}
