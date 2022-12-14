013af04744a883e441180ce95b7fe30b2d5dcd2f
TeeGodwin <toksgodwin@gmail.com>
2022-12-14 00:11:31 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,13 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -21,6 +19,4 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
-
-
 }
