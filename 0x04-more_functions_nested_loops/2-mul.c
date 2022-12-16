ed2eeedf9cb72e90321b8c22dfd82419334bf1f6
TeeGodwin <toksgodwin@gmail.com>
2022-12-16 02:42:07 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 nums
+ * @a: first param
+ * @b: second param
+ * Return: something
+ */
+
+int mul(int a, int b);
+{
+	int a = 10;
+	int b = 15;
+	
+	return (a * b);
+}
