41a0c914deb19edc3ff35998ff2ff9687d6acd41
TeeGodwin <toksgodwin@gmail.com>
2022-12-16 08:18:02 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,22 +4,33 @@
  * main - fizbuzz
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
 
 	for (i = 1; 1 < 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("fizzbuzz ");
-		else if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
+		{
 			printf("fizz ");
-		else if (i % 5 == 0)
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
 			printf("buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("fizzbuzz ");
+		}
+		else if (i == 1)
+		{
+			printf("1 ");
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 	}
-	printf("buzz\n");
+	printf("\n");
 	return (0);
 }
