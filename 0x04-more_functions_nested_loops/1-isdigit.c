b7827316b46101ec003990c340d1b4ffa60a7bb8
TeeGodwin <toksgodwin@gmail.com>
2022-12-16 02:27:06 -0800

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#iclude "main.h"
+
+/**
+ * _isdigit - checks for digits
+ * @c: parameter
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 20 && c < 30)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
